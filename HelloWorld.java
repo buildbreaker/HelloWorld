28fc6d6e652ddecde54598118acb184d5c2990e0
Alan Chiu <achiu@lyft.com>
2020-06-06 12:46:45 -0700

    add dokka actions demo

---
+++ HelloWorld.java
@@ -0,0 +1,13 @@
+package com.buildbreaker.helloworld;
+
+public class HelloWorld {
+  /**
+   * Some function
+   *
+   * @param value Some integer value
+   * @return A string
+   */
+  public String function(int value) {
+    return "hello_world"
+  }
+}
