04c84d9c84153476af76762a44e00c367a917a88
Alan Chiu <awlchiu@gmail.com>
2022-11-16 13:11:22 -0800

    simple example

--- BUILD
+++ BUILD
@@ -1,6 +1,10 @@
+load("@io_bazel_rules_kotlin//kotlin:jvm.bzl", "kt_jvm_library")
 
-java_library(
+kt_jvm_library(
     name = "hello",
     srcs = ["Hello.java"],
-    deps = [],
+    deps = [
+        "@maven//build_buf_connect_library_pt",
+        "//gen:generated_sources"
+    ],
 )
---
+++ Hello.kt
@@ -0,0 +1,29 @@
+package buildbreaker
+
+import buf.connect.kotlin.ConnectClientOption
+import buf.connect.kotlin.ConnectOkHttpClient
+import buf.connect.kotlin.ProtoClientOption
+import buf.connect.kotlin.ProtocolClient
+import com.buf.connect.demo.eliza.v1.ElizaServiceClient
+import com.buf.connect.demo.eliza.v1.SayRequest
+import okhttp3.OkHttpClient
+
+fun connect() {
+    val client = ProtocolClient(
+        target = "https://demo.connect.build",
+        httpClient = ConnectOkHttpClient(
+            OkHttpClient()
+            .newBuilder()
+            .build()
+        ),
+        ConnectClientOption(),
+        ProtoClientOption(),
+    )
+
+    val elizaServiceClient = ElizaServiceClient(client)
+
+    val response = elizaServiceClient.say(SayRequest(sentence = "Hello!"))
+
+    println(response?.message.sentence)
+
+}
