fb22f87c8f45f3a614853147e5ec2f46eda20c0e
Alan Chiu <awlchiu@gmail.com>
2022-11-22 15:05:49 -0800

    cu

--- plugin/Main.kt
+++ plugin/Main.kt
@@ -1,12 +1,35 @@
 package buildbreaker.plugin
 
-import buildbreaker.plugin.generator.Plugin
+import com.google.protobuf.compiler.CodeGeneratorRequest
+import com.google.protobuf.compiler.CodeGeneratorResponse
+import okio.Buffer
+import java.io.IOException
+import java.io.InputStream
+import java.io.OutputStream
+import java.lang.RuntimeException
 
 class Main {
     companion object {
+
+        private val inputStream: InputStream = System.`in`
+        private val outputStream: OutputStream = System.out
         @JvmStatic
         fun main(args: Array<String>) {
-            Plugin.run()
+            val request = try {
+                val inputStream = inputStream
+                val rawRequest = Buffer().readFrom(inputStream).readByteString()
+                CodeGeneratorRequest.ADAPTER.decode(rawRequest)
+            } catch (e: IOException) {
+                throw PluginException("protoc sent unparseable request to plugin.", e)
+            }
+            val response = CodeGeneratorResponse(
+                file_ = request.file_to_generate.map { name ->
+                    CodeGeneratorResponse.File(name, content = "file contents for $name")
+                }
+            )
+            outputStream.write(response.encode())
+            outputStream.flush()
         }
     }
+    class PluginException(message: String?, cause: Throwable?) : RuntimeException(message, cause)
 }
